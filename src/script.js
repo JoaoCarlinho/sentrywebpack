e6040dbad92cd15f20d85efb9bdcd6b8153feabf
Owner <owner@Owners-MacBook-Pro.local>
2018-12-10 16:58:24 -0600

    webpack rebuilt

--- src/script.js
+++ src/script.js
@@ -2,8 +2,8 @@ let secondMessage = require('./script2');
 let message = "this is a message from the file 'script.js'";
 alert(message);
 function CauseError(){
-    //document.getElementById('error').innerHTML = 'this will cause an error';
-    router.get('/nonexistent.js');
+    document.getElementById('error').innerHTML = 'this will cause an error';
+    //router.get('/nonexistent.js');
 
 }
 console.log(secondMessage);
